da1eb99c14b7c785f2935c25ba994ad2d848cfb6
XiongTom <1243002874@qq.com>
2021-09-23 21:45:19 +0800

    you can type remark here

--- main.cpp
+++ main.cpp
@@ -58,28 +58,34 @@ void countwords(){
 }
 void findswitchcase(){
 	string line;
-	int f=-1;
+	int f=0,flag=0;              
 	size_t sfind,cfind;
-	int s=0,c[100]={0};
+	int s=0,c[100]={0};             //s is for counting switch; c[100] is for counting case;
 	for(int i=0;i<getl.size();i++){
 		line=getl[i];
 		sfind=line.find("switch");
 		cfind=line.find("case");
 		if(sfind!=line.npos){
 			s=s+1;
-			f=f+1;
+		    flag=1;
 		}
-		if((cfind!=line.npos)&&(f>=0)){
-			c[f]+=1; 
+		if((cfind!=line.npos)&&(flag==1)){    //if there is one "switch" then we count the case number in that "switch"
+			c[f]+=1;
 		}
+		if(line.find("default")!=line.npos){
+			    flag==0;    
+			    f=f+1;
+		}
+
 	}
-	cout<<"switch num:"<<s<<endl;
+	cout<<"switch num:"<<s<<endl;         
 	cout<<"case num:";
-	for(int i=0;i<=f;i++){
+	for(int i=0;i<f;i++){                //output the num of case in each switch 
 		cout<<" "<<c[i];
-		cout<<endl;
-	}
+		}
+	cout<<endl;
 }
+
 void findifelse(int level){
     int sum1=0,sum2=0;
     string line;
--- switch-case.cpp
+++ switch-case.cpp
@@ -3,7 +3,7 @@
 #include<cstring>
 void findswitchcase(){
 	string line;
-	int f=-1;               
+	int f=0,flag=0;              
 	size_t sfind,cfind;
 	int s=0,c[100]={0};             //s is for counting switch; c[100] is for counting case;
 	for(int i=0;i<getl.size();i++){
@@ -12,17 +12,21 @@ void findswitchcase(){
 		cfind=line.find("case");
 		if(sfind!=line.npos){
 			s=s+1;
-			f=f+1;
+		    flag=1;
 		}
-		if((cfind!=line.npos)&&(f>=0)){    //if there is one "switch" then we count the case number in that "switch"
-			c[f]+=1; 
+		if((cfind!=line.npos)&&(flag==1)){    //if there is one "switch" then we count the case number in that "switch"
+			c[f]+=1;
 		}
+		if(line.find("default")!=line.npos){
+			    flag==0;    
+			    f=f+1;
+		}
+
 	}
 	cout<<"switch num:"<<s<<endl;         
 	cout<<"case num:";
-	for(int i=0;i<=f;i++){                //output the num of case in each switch 
+	for(int i=0;i<f;i++){                //output the num of case in each switch 
 		cout<<" "<<c[i];
-		cout<<endl;
-	}
+		}
+	cout<<endl;
 }
-
