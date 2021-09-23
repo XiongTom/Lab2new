550cb0ecfcea1650176d626c683e27f48742ccc7
XiongTom <1243002874@qq.com>
2021-09-23 17:09:45 +0800

    you can type remark here

--- clear.cpp
+++ clear.cpp
@@ -7,22 +7,22 @@ void clear(){
 	int i=0;
 	size_t a=0,k=0;    
 	for(i=0;i<getl.size();i++){
-		line=getl[i];              //Each row is stored in line
+		line=getl[i];              //each row is stored in line
 		if(line.find("//")!=line.npos){
 			a=line.find("//");      
-			if(a==0){              //If the // annotation is at the beginning, delete the entire line of comments 
+			if(a==0){              //if the // annotation is at the beginning, delete the entire line of comments 
 				getl.erase(getl.begin()+i);
 				i-=1;
 			}else{
-				getl[i]=line.substr(0,a);      //If not, save everything before the // symbol 
+				getl[i]=line.substr(0,a);      //if not, save everything before the // symbol 
 			}
 		}else if(line.find("/*")!=line.npos){
 			a=line.find("/*");
 			k=line.find("*/");
-			if(k!= line.npos){                //If the annotation is on a line, save the content up to /*
+			if(k!= line.npos){                //if the annotation is on a line, save the content up to /*
 				getl[i]=line.substr(0,a);
 			}else{
-				getl[i]=line.substr(0,a);     //If it is a multi-line comment, delete it line by line
+				getl[i]=line.substr(0,a);     //if it is a multi-line comment, delete it line by line
 				a=i+1;
 				line=getl[a];
 				while(line.find("*/"==line)){
@@ -32,7 +32,7 @@ void clear(){
 				i=a; 
 			}
 		}else if(line.find("\"")!=line.npos){
-			size_t s[100];                    //Store the location where "appears
+			size_t s[100];                    //store the location where "appears
 			int x=0,y=0;
 			while((a=line.find("\"",x))!=line.npos){
 				s[y]=a;
